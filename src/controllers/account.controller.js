514af0be4a0603681251d066dc0e22b7d41dcb32
Swayam Gupta <swayamgupta193@gmail.com>
2026-02-24 19:25:58 +0530

    added account balance fetch

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,5 +1,6 @@
 const accountModel = require("../models/account.model")
 
+
 async function createAccount(req,res) {
     const user = req.user;
 
@@ -12,4 +13,28 @@ async function createAccount(req,res) {
     })
     
 }
-module.exports= {createAccount}
+
+async function getAccountBalanceController(req,res) {
+    const{accountID} = req.params
+
+    const account = await accountModel.findOne({
+        _id :accountID,
+        user :req.user._id
+    })
+    if(!account){
+        return res.status(400).json({
+            message:"Account not found"
+        })
+    }
+    const balance = await account.getBalance()
+    
+    res.status(200).json({
+        accountID : account._id,
+        balance:balance
+    })
+
+    
+}
+
+
+module.exports= {createAccount,getAccountBalanceController}
